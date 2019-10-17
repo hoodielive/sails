896221717b94a6230834152fe2a95e0c910d4f6a
hoodielive <hoodielive@gmail.com>
2019-10-16 20:49:04 -0400

    Generated skel and cats controller

---
+++ app/api/controllers/CatsController.js
@@ -0,0 +1,12 @@
+/**
+ * CatsController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
